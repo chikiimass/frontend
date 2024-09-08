2bcff97e9b89d97a07c60b392b285044e469c6bc
chikiimass <chikiimass@gmail.com>
2024-09-08 13:56:51 +0000

    info

--- src/app/(frontend)/info/page.tsx
+++ src/app/(frontend)/info/page.tsx
@@ -1,12 +1,18 @@
+import { getPayloadHMR } from '@payloadcms/next/utilities';
 import React from 'react'
+import configPromise from '@payload-config'
 
-const page = ({ params }) => {
+
+const page = async ({ params }) => {
     const payload = await getPayloadHMR({ config: configPromise });
     const data = await payload.find({
         collection: 'movies',
+        where: {
+          slug: { equals: `${params.slug}` },
+        },
     })
   return (
-    <div>page</div>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 

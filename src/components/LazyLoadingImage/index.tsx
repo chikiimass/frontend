9b3078b9b3df328f9efb919efc1b8413b8903f98
hooleymass <hockleymasese3@gmail.com>
2024-09-09 22:42:27 +0300

    h

--- src/components/LazyLoadingImage/index.tsx
+++ src/components/LazyLoadingImage/index.tsx
@@ -1,5 +1,6 @@
 import cn from '@/utils/cn';
 import { cva, type VariantProps } from 'class-variance-authority';
+import Image from 'next/image';
 import React from 'react';
 
 const LazyLoadedImageStyles = cva(['font-semibold'], {
@@ -8,47 +9,39 @@ const LazyLoadedImageStyles = cva(['font-semibold'], {
       small: 'rounded-sm',
       medium: 'rounded-md',
       large: 'rounded-lg',
-      none: 'rounded-none',
-    },
+      none: 'rounded-none'
+
+    }
   },
   defaultVariants: {
-    radius: 'medium',
-  },
-});
+    radius: 'medium'
+  }
+})
 
-type LazyLoadedImageProps = React.ComponentPropsWithoutRef<'div'> &
+type LazyLoadedImageprops = React.ComponentPropsWithoutRef<'div'> &
   VariantProps<typeof LazyLoadedImageStyles> & {
-    image?: string | null;
-    name: string;
-    width?: number;
-    height?: number;
-  };
-
-const LazyLoadedImage: React.FC<LazyLoadedImageProps> = ({
-  image,
-  name,
-  width = 640, // Default width
-  height = 360, // Default height
-  className,
-  ...props
-}) => {
+    image?: string | null
+    name: string
+  }
+const LazyLoadedImage: React.FC<LazyLoadedImageprops> = ({ image, name, className, ...props }) => {
   return (
-    <div className={cn(className, LazyLoadedImageStyles({ radius: props.radius }))}>
+    <div >
       {image ? (
-        <img
+
+        <Image
           src={image}
           alt={name}
-          width={width}
-          height={height}
-          loading="lazy" // Enable lazy loading
-          className={cn('w-full h-auto', LazyLoadedImageStyles({ radius: props.radius }), className)}
-          {...props}
+          layout="responsive"
+          width={16}
+          height={9}
+          priority
+          {...props} className={cn(LazyLoadedImageStyles(props), className)}
         />
       ) : (
-        <span className="text-xs font-bold">{name}</span>
+        <span className="text-xs font-bold"></span>
       )}
     </div>
   );
 };
 
-export default LazyLoadedImage;
+export default LazyLoadedImage
